1373bfb71f7767d0e88b8ebf6f5eadc5e203965e
GhoZaa <ghoza16m@gmail.com>
2022-12-08 06:11:55 +0700

    create header component and adjust the styles

--- segar-web-main/src/App.js
+++ segar-web-main/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
 import Navbar from './components/Navbar';
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <div className='App'>
+      <Navbar />
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ segar-web-main/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div id='main'>
+      <div className='name'>
+        <h1>
+          <span>Free</span> Workout Plans
+        </h1>
+        <p className='details'>
+          Tutorial workout yang dapat diakses kapanpun dan dimanapun. Bergabung
+          untuk pengalaman kesehatan yang lebih baik.
+        </p>
+        <div className='header-btns'>
+          <button href='#' className='header-btn'>
+            JOIN US
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
